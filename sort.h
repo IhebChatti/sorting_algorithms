bd6c16b2f353f87d770d8addd1a038f246a8fdc4
IhebChatti <iheb.chatti@holbertonschool.com>
2020-06-15 13:54:54 +0100

    added function prototype

--- sort.h
+++ sort.h
@@ -31,5 +31,6 @@ void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 int partition(int *array, size_t size, int low, int high);
 void lomuto(int *array, size_t size, int low, int high);
+void shell_sort(int *array, size_t size);
 
 #endif
